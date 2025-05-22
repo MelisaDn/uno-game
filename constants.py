47b2ea92f7ca826b6da575430ee96b77da2d130f
Neslihan Cesur <137646798+neslihancesurr@users.noreply.github.com>
2025-05-22 23:32:11 +0300

    comments on constants.py

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
 import pygame
-# Initialize pygame
+
 pygame.init()
 
-# Constants
+# set the constants for main frame
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 600
 CARD_WIDTH = 100
 CARD_HEIGHT = 150
 CARD_SPACING = 30
 
-# Colors
+# set the color values
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 128, 0)
@@ -18,11 +18,11 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 GRAY = (128, 128, 128)
 
-# Create the screen
+# display the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Uno Game - 4 Players")
 
-# Card colors and values
+# list out all possible colors and number values
 COLORS = ["red", "blue", "green", "yellow"]
 VALUES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "draw2"]
 SPECIAL_CARDS = ["wild", "wild_draw4"]
